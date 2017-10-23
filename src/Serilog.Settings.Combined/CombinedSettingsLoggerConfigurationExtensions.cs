8bf53d42b16236c4f4eb085b75af221c282b0441
Thibaud DESODT <thibaud.desodt@dgtresor.gouv.fr>
2017-10-23 21:50:58 +0200

    Add xml docs
    relatd to #1 and [

--- src/Serilog.Settings.Combined/CombinedSettingsLoggerConfigurationExtensions.cs
+++ src/Serilog.Settings.Combined/CombinedSettingsLoggerConfigurationExtensions.cs
@@ -4,8 +4,17 @@ using Serilog.Settings.Combined;
 
 namespace Serilog
 {
+    /// <summary>
+    /// Extensions for combined sources of key-value settings
+    /// </summary>
     public static class CombinedSettingsLoggerConfigurationExtensions
     {
+        /// <summary>
+        /// Configure the logger from multiple sources of settings specified in the Serilog key-value setting format.
+        /// </summary>
+        /// <param name="lsc">The settings configuration object.</param>
+        /// <param name="build">the operations to execute on the builder to configure other sources of settings.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
         public static LoggerConfiguration Combined(this LoggerSettingsConfiguration lsc, Func<IConfigBuilder, IConfigBuilder> build)
         {
             var configBuilder = new ConfigBuilder();
--- src/Serilog.Settings.Combined/Settings/Combined/IConfigBuilder.cs
+++ src/Serilog.Settings.Combined/Settings/Combined/IConfigBuilder.cs
@@ -2,8 +2,16 @@
 
 namespace Serilog.Settings.Combined
 {
+    /// <summary>
+    /// The entry point for fluent combination of multiple source of settings
+    /// </summary>
     public interface IConfigBuilder
     {
+        /// <summary>
+        /// Adds a source of key-value settings to the builder, after all the previously declared sources
+        /// </summary>
+        /// <param name="source">an enumerable collection of settings</param>
+        /// <returns>the builder object to allow chaining</returns>
         IConfigBuilder AddSource(IEnumerable<KeyValuePair<string, string>> source);
     }
 }
